2d440670723de49df291f85ca775316915eb1744
Sellto <tom.selleslagh@gmail.com>
2021-05-10 21:15:19 +0200

    Pre V1

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-    lib "github.com/cetic/kubeedge-mapper/internal"
-    log "github.com/sirupsen/logrus"
-    "os"
-
-    //"os"
+	"github.com/cetic/kubeedge-mapper/internal/device"
+	log "github.com/sirupsen/logrus"
+	"os"
+	//"os"
 )
 
-func main(){
-  device := lib.Device{}
-  device.GetConfigFromFile(os.Args[1])
-  log.Infof("%+v",device)
-  device.Listen()
+func main() {
+	d := device.Device{}
+	d.GetConfigFromFile(os.Args[1])
+	log.Infof("%+v", d)
+	d.Listen()
 }
